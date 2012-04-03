72874257b0e8df0b9891235caa6fa65091b1ffd0
Reorx <reorx@nodemix.com>
2012-04-03 18:55:22 +0800

    db/ -> mongodb/, rdbs/, connections.py;add flask_style_exmple.py

--- torext/db/__init__.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
-#
-# NOTE. this folder, would better be a sepratable module from torext,
-# and contains nothing about tornado functions and classes
-
-
-from .connections import Connections
