d74ee74b64793f695270f7c2990cf9eac9848c52
reorx <novoreorx@gmail.com>
2015-12-24 01:37:57 +0800

    refine setup.py, add requirements files

--- setup.py
+++ setup.py
@@ -1,13 +1,26 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from setuptools import setup
+
+
 # Always stay on 0.9 before documentation is finished
 __version__ = '0.9.4-r5'
 
-from setuptools import setup
 
-with open('README.rst', 'r') as f:
-    description = f.read()
+def get_requires():
+    with open('requirements.txt', 'r') as f:
+        requires = [i for i in map(lambda x: x.strip(), f.readlines()) if i]
+    return requires
+
+
+def get_long_description():
+    try:
+        with open('README.md', 'r') as f:
+            return f.read()
+    except IOError:
+        return ''
+
 
 setup(
     license='License :: OSI Approved :: MIT License',
@@ -17,7 +30,7 @@ setup(
     author_email='novoreorx@gmail.com',
     url='http://github.com/reorx/torext',
     description="The missing tornado mate",
-    long_description=description,
+    long_description=get_long_description(),
     packages=[
         'torext',
         'torext.handlers',
@@ -36,7 +49,5 @@ setup(
             'torext-sketch = torext.scripts.sketch:main',
         ]
     },
-    install_requires=[
-        'tornado>=3.0'
-    ],
+    install_requires=get_requires(),
 )
